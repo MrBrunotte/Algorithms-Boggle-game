791a11ffe1059624433b370cc98370cd6329903b
mrbrunotte <mrbrunotte@gmail.com>
2019-07-28 13:39:54 +0200

    Bring it all together

--- boggle/boggle.py
+++ boggle/boggle.py
@@ -8,8 +8,8 @@ def make_grid(width, height):
     for a boggle game
     """
     return {(row, col): choice(ascii_uppercase)
-            for row in range(height)
-            for col in range(width)}
+        for row in range(height)
+        for col in range(width)}
 
 
 def neighbours_of_position(coords):
@@ -32,7 +32,7 @@ def neighbours_of_position(coords):
     right = (row, col + 1)
 
     # Bottom-left to bottom-right
-    bottom_left = (row + 1, col - 1)
+    bottom_left = (row + 1, col -1)
     bottom_center = (row + 1, col)
     bottom_right = (row + 1, col + 1)
 
@@ -75,10 +75,10 @@ def search(grid, dictionary):
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
-
+    
     for position in grid:
         do_search([position])
-
+    
     words = []
     for path in paths:
         words.append(path_to_word(grid, path))
@@ -91,3 +91,19 @@ def get_dictionary(dictionary_file):
     """
     with open(dictionary_file) as f:
         return [w.strip().upper() for w in f]
+
+
+def main():
+    """
+    This is the function that will run the whole project
+    """
+    grid = make_grid(3, 3)
+    dictionary = get_dictionary('words.txt')
+    words = search(grid, dictionary)
+    for word in words:
+        print(word)
+    print("Found %s words" % len(words))
+
+
+if __name__ == "__main__":
+    main()
