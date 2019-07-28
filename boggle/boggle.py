56adb30bd0cfb9d6f8adc878397bdc21aabd189e
mrbrunotte <mrbrunotte@gmail.com>
2019-07-28 13:27:02 +0200

    Words

--- boggle/boggle.py
+++ boggle/boggle.py
@@ -8,8 +8,8 @@ def make_grid(width, height):
     for a boggle game
     """
     return {(row, col): choice(ascii_uppercase)
-        for row in range(height)
-        for col in range(width)}
+            for row in range(height)
+            for col in range(width)}
 
 
 def neighbours_of_position(coords):
@@ -32,7 +32,7 @@ def neighbours_of_position(coords):
     right = (row, col + 1)
 
     # Bottom-left to bottom-right
-    bottom_left = (row + 1, col -1)
+    bottom_left = (row + 1, col - 1)
     bottom_center = (row + 1, col)
     bottom_right = (row + 1, col + 1)
 
@@ -51,3 +51,43 @@ def all_grid_neighbours(grid):
         position_neighbours = neighbours_of_position(position)
         neighbours[position] = [p for p in position_neighbours if p in grid]
     return neighbours
+
+
+def path_to_word(grid, path):
+    """
+    Add all of the letters on the path to a string
+    """
+    return ''.join([grid[p] for p in path])
+
+
+def search(grid, dictionary):
+    """
+    Search thrugh the paths to locate words by matching
+    strings to words in a dictionary
+    """
+    neighbours = all_grid_neighbours(grid)
+    paths = []
+
+    def do_search(path):
+        word = path_to_word(grid, path)
+        if word in dictionary:
+            paths.append(path)
+        for next_pos in neighbours[path[-1]]:
+            if next_pos not in path:
+                do_search(path + [next_pos])
+
+    for position in grid:
+        do_search([position])
+
+    words = []
+    for path in paths:
+        words.append(path_to_word(grid, path))
+    return set(words)
+
+
+def get_dictionary(dictionary_file):
+    """
+    Load dictionary file
+    """
+    with open(dictionary_file) as f:
+        return [w.strip().upper() for w in f]
